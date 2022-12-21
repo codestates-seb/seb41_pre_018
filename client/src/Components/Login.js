dc90c1a0c95c03a8e552c5e227203ebca059e96a
minseob shim <shim5505@gmail.com>
2022-12-22 00:03:23 +0900

    [FE_Feat] 기본 Form 구성

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,3 +1,40 @@
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+
+const Login_Wrapper = styled.div`
+  margin: 0 auto;
+  height: 500px;
+  width: 500px;
+  border: solid red 1px;
+
+  .Login_Form {
+    margin: 0 auto;
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
 export default function Login() {
-  return <div>로그인 해주세요</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  // const emailRegex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+
+  return (
+    <Login_Wrapper>
+      <form className="Login_Form" onSubmit={handleSubmit(onSubmit)}>
+        <input placeholder="아이디를 입력해주세요" {...register('id')} />
+        <input
+          placeholder="비밀번호를 입력해주세요"
+          {...register('exampleRequired', { required: true })}
+        />
+        {errors.exampleRequired && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
+    </Login_Wrapper>
+  );
 }
