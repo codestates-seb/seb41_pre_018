4908121656a010d9101be1acbadf755ae1d383da
maverickhg <doncic1990@gmail.com>
2022-12-24 21:19:49 +0900

    Answer 의 Controller 입니다. getAnswers 메서드에만 오류가 있으나 전체적으로 확인할 필요가 있습니다.

--- server/src/main/java/sebpre018/com/stackOverflowClone/answer/controller/AnswerController.java
+++ server/src/main/java/sebpre018/com/stackOverflowClone/answer/controller/AnswerController.java
@@ -4,45 +4,68 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.w3c.dom.Text;
+import sebpre018.com.stackOverflowClone.answer.dto.AnswerDto;
 import sebpre018.com.stackOverflowClone.answer.dto.AnswerPatchDto;
 import sebpre018.com.stackOverflowClone.answer.dto.AnswerPostDto;
+import sebpre018.com.stackOverflowClone.answer.dto.AnswerResponseDto;
+import sebpre018.com.stackOverflowClone.answer.entity.Answer;
+import sebpre018.com.stackOverflowClone.answer.mapper.AnswerMapper;
+import sebpre018.com.stackOverflowClone.answer.service.AnswerService;
 
 import javax.validation.Valid;
-import java.math.BigInteger;
-import java.sql.Blob;
-import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/answers")
 @Validated
 public class AnswerController {
-    @PostMapping
-    public ResponseEntity postAnswer(@Valid @RequestBody AnswerPostDto answerPostDto){
+    private final AnswerService answerService;
+    private final AnswerMapper mapper;
 
-        return new ResponseEntity<>(answerPostDto, HttpStatus.CREATED);
+    public AnswerController(AnswerService answerService, AnswerMapper mapper) {
+        this.answerService = answerService;
+        this.mapper = mapper;
+    }
+
+    @PostMapping
+    public ResponseEntity postAnswer(@Valid @RequestBody AnswerDto.Post requestBody){
+
+        Answer answer = mapper.answerPostDtoToAnswer(requestBody);
+        Answer createdAnswer = answerService.createAnswer(answer);
+
+        return new ResponseEntity<>(mapper.answerToAnswerResponseDto(createdAnswer), HttpStatus.CREATED);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity patchAnswer(@PathVariable("id") BigInteger id,
-                                      @Valid @RequestBody AnswerPatchDto answerPatchDto) {
-        answerPatchDto.setAnswerId(id);
+    public ResponseEntity patchAnswer(@PathVariable("id") Long answerId,
+                                      @Valid @RequestBody AnswerDto.Patch requestBody) {
+        requestBody.setAnswerId(answerId);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        Answer answer = answerService.updateAnswer(mapper.answerPatchDtoToAnswer(requestBody));
+
+        return new ResponseEntity<>(mapper.answerToAnswerResponseDto(answer), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getAnswer(@PathVariable("id") BigInteger id) {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity getAnswer(@PathVariable("id") Long answerId) {
+
+        Answer answer = answerService.findAnswer(answerId);
+
+        return new ResponseEntity<>(mapper.answerToAnswerResponseDto(answer), HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity getAnswers() {
+        List<AnswerResponseDto> response = answers.stream()
+                .map(answer -> mapper.answerToAnswerResponseDto(answers))
+                .collect(Collectors.toList());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteAnswer(@PathVariable("id") BigInteger id) {
+    public ResponseEntity deleteAnswer(@PathVariable("id") Long answerId) {
+        answerService.deleteAnswer(answerId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
