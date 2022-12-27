4598b79fb3dd1f353c653651a53c87182f722d3d
Erin A. Yoo <110885007+ahnseoYoo@users.noreply.github.com>
2022-12-27 09:30:10 +0900

    Revert "Features/signup completed"

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,8 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
 import './App.css';
-import Navbar from './Components/Navbar';
-// import Question from './Components/Question';
-import Login from './Components/Login';
 import Main from './Pages/Main';
-import Sign from './Pages/Sign';
-import MyPage from './Pages/MyPage';
-import Signup_Completed from './Pages/Signup_Completed';
-import Search from './Pages/Search';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #f1f2f3;
-  }
-`
-let isLoggedIn = false;
 
 function App() {
-  return (
-    <BrowserRouter>
-      <GlobalStyle/>
-      <Navbar isLoggedIn={isLoggedIn} />
-      <Routes>
-        <Route path="/" element={<Main isLoggedIn={isLoggedIn}/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Sign />}/>
-        <Route path="/user/:name" element={<MyPage />} />
-        <Route path="/signup-completed" element={<Signup_Completed />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Main />;
 }
 
 export default App;
