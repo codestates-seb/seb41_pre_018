d3084c3056ef581aa7d247a13c45245baef19f9a
jungdonggyu <1013as@naver.com>
2022-12-27 19:15:56 +0900

    Rename: "Answer, Comment 레포지토리 메소드 추가" QuestionId값을 이용해 값을 읽어올 수 있도록 각 레포지토리에 JPQL문을 사용, 메서드를 추가하였습니다

--- server/src/main/java/sebpre018/com/stackOverflowClone/answer/repository/AnswerRepository.java
+++ server/src/main/java/sebpre018/com/stackOverflowClone/answer/repository/AnswerRepository.java
@@ -1,10 +1,16 @@
 package sebpre018.com.stackOverflowClone.answer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import sebpre018.com.stackOverflowClone.Tag.entity.Tag;
 import sebpre018.com.stackOverflowClone.answer.entity.Answer;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
     Optional<Answer> findByAnswer(Long answerId);
+
+    @Query(value = "SELECT t FROM Tag t WHERE t.questionId = :questionId")
+    List<Answer> findAllByQuestionId(long questionId);
 }
