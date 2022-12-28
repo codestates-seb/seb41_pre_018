9fb94768b72ded7a81236503ced54f8db2eb14ae
ahn <201717504@kyonggi.ac.kr>
2022-12-29 03:23:01 +0900

    "createAt,modifiedAt 추가"

--- server/src/main/java/sebpre018/com/stackOverflowClone/member/dto/MemberResponseDto.java
+++ server/src/main/java/sebpre018/com/stackOverflowClone/member/dto/MemberResponseDto.java
@@ -2,6 +2,9 @@ package sebpre018.com.stackOverflowClone.member.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import sebpre018.com.stackOverflowClone.member.entity.Member;
+
+import java.time.LocalDateTime;
 
 @Builder
 @Getter
@@ -11,4 +14,7 @@ public class MemberResponseDto {
     private String username;
     private String password;
     private String aboutMe;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
 }
