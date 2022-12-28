9a36ba097c94f8b9f17725cb34bb6d84796fa892
jungdonggyu <1013as@naver.com>
2022-12-28 22:34:06 +0900

    Rename : "JPQL 수정" 제가 JPQL로 해결책을 찾지 못해서 nativeQuery로 변경하였습니다 ㅠㅠ

--- server/src/main/java/sebpre018/com/stackOverflowClone/comment/repository/CommentRepository.java
+++ server/src/main/java/sebpre018/com/stackOverflowClone/comment/repository/CommentRepository.java
@@ -9,8 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Optional<Comment> findByComment(Long commentId);
 
-    @Query(value = "SELECT t FROM Tag t WHERE t.questionId = :questionId")
+    @Query(value = "select * from comment where question_id = :questionId", nativeQuery = true)
     List<Comment> findAllByQuestionId(long questionId);
 }
