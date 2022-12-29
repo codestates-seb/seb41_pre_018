a362bf46d741bd1c032f7048f18277eecf89f1de
ahn <201717504@kyonggi.ac.kr>
2022-12-29 22:16:46 +0900

    "Member 수정"

---
+++ server/src/main/java/sebpre018/com/stackOverflowClone/member/assembler/MemberAssembler.java
@@ -0,0 +1,24 @@
+package sebpre018.com.stackOverflowClone.member.assembler;
+
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.stereotype.Component;
+import sebpre018.com.stackOverflowClone.member.controller.MemberController;
+import sebpre018.com.stackOverflowClone.member.entity.Member;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class MemberAssembler implements RepresentationModelAssembler<Member, EntityModel<Member>> {
+
+    @Override
+    public EntityModel<Member> toModel(Member member) {
+        return EntityModel.of(member,
+                WebMvcLinkBuilder.linkTo(methodOn(MemberController.class).getMember(member.getId())).withSelfRel(),
+                linkTo(methodOn(MemberController.class).getMembers()).withRel("members"));
+    }
+
+}
--- server/src/main/java/sebpre018/com/stackOverflowClone/member/dto/MemberDto.java
+++ server/src/main/java/sebpre018/com/stackOverflowClone/member/dto/MemberDto.java
@@ -12,14 +12,13 @@ public class MemberDto {
     @Getter
     public static class Post{
         @NotBlank
-        @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
         @Email
         private String email;
         @NotBlank
         @Pattern(regexp = "[A-Za-z0-9가-힇]{2,20}")
         private String username;
         @NotBlank
-        @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@!%*#?&])[A-Za-z\\d@!%*#?&]{8,}$")
         private String password;
         @NotBlank
         private String aboutMe;
